cb61994a9496ee4eb3c3839ffc9106f6bc15711a
Antoni Boucher <bouanto@zoho.com>
2026-08-25 20:50:49 -0400

    Do not panic on warnings

--- gccjit_sys/src/lib.rs
+++ gccjit_sys/src/lib.rs
@@ -838,4 +838,7 @@ extern_maybe_dlopen! {
     #[cfg(feature="master")]
     fn gcc_jit_block_get_successor(block: *mut gcc_jit_block,
                                    index: c_int) -> *mut gcc_jit_block;
+
+    #[cfg(feature="master")]
+    fn gcc_jit_context_get_error_count(ctx: *mut gcc_jit_context) -> c_int;
 }
